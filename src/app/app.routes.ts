2e2143ca7a8d29e173057148761ed27821aef253
Binyam Fitamo <binyamfitamo7@gmail.com>
2025-08-21 15:50:07 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BookAppointmentComponent } from './pages/book-appointment/book-appointment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'book', component: BookAppointmentComponent },
+];
